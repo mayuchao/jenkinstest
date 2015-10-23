4c2dac66c4ba2d673a7e9a1a09c1b8e57a131724
mayuchao <mayuchaobupt@163.com>
2015-10-23 11:27:44 +0800

    123

--- src/WebDriver/Page/testlist.java
+++ src/WebDriver/Page/testlist.java
@@ -2,7 +2,6 @@ package WebDriver.Page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class testlist {
